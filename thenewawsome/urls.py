2c51dd9c8fefa80001116e61c0a9ee92522425f2
flavius <flavius@localhost>
2016-11-13 19:38:53 +0200

    Yes, I have commited

--- thenewawsome/urls.py
+++ thenewawsome/urls.py
@@ -17,9 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from thenewawsome.views import hello_world, root_page, random_number
 
+from django.conf.urls import include
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^helloworld/$', hello_world),
     url(r'^random/(\d+)/$', random_number),
+    url(r'^polls/', include('polls.urls')),
     url(r'^.?', root_page),
 ]
