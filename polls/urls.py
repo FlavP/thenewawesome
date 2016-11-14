f0a472b09cc483aa5acc12b824d88e512205415d
flavius <flavius.popoiu@mediaimage.ro>
2016-11-14 16:48:49 +0200

    mai avansam un pic

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index/$', views.index),
+    #url(r'^deeper/$', views.some_else),
     ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Question, Choice
 
 # Create your views here.
+'''
 def index(request):
     return HttpResponse("You are in the polls index")
+
+def some_else(request):
+    return HttpResponse("You are one step deeper")
+'''
+'''
+def index(request):
+    #Ultimele 5 intrebari sortate pe data
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #le insiram in output cu , intre ele
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+But then again, this is not The Way, we need to create templates, so we
+create a "templates" folder in pols
+'''
