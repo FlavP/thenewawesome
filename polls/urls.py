a64dedfe5c80a157ca69a99e978689e38e4ed182
flavius <flavius.popoiu@mediaimage.ro>
2016-11-14 17:53:52 +0200

    mai avansam un pic

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,16 @@ from django.conf.urls import url
 
 from . import views
 from django.conf.urls import include
-
+    # 3 Add the 3 views
+    # The data surrounded by parentheses is sent to the function
+    # ?P<question_id> defines the name for the data within
+    # the parentheses
+    # [0-9]+ says we will match 1 or more numbers
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^index/$', views.index),
+    #url(r'^index/$', views.index),
+    url(r'^(?P<question_id>[0-9]+)/$', views.details, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results')
     #url(r'^deeper/$', views.some_else),
     ]
+#minutul 7:27
--- polls/views.py
+++ polls/views.py
@@ -3,20 +3,22 @@ from django.shortcuts import get_object_or_404
 from .models import Question, Choice
 
 # Create your views here.
-'''
+
 def index(request):
     return HttpResponse("You are in the polls index")
 
-def some_else(request):
-    return HttpResponse("You are one step deeper")
-'''
-'''
 def index(request):
     #Ultimele 5 intrebari sortate pe data
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #le insiram in output cu , intre ele
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
-But then again, this is not The Way, we need to create templates, so we
-create a "templates" folder in pols
-'''
+
+def details(request, question_id):
+    return HttpResponse("You're looking at the question %s" % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the response of question %s"
+    return HttpResponse(response % question_id)
+#But then again, this is not The Way, we need to create templates, so we
+#create a "templates" folder in pols
