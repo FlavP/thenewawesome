beaea07554a4a5a4e22ec96fafb6188767dfedc5
flavius <flavius.popoiu@mediaimage.ro>
2016-11-15 13:29:42 +0200

    Finished part 3

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 from django.conf.urls import include
+
+app_name = 'polls'
     # 3 Add the 3 views
     # The data surrounded by parentheses is sent to the function
     # ?P<question_id> defines the name for the data within
@@ -10,8 +12,9 @@ from django.conf.urls import include
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     #url(r'^index/$', views.index),
-    url(r'^(?P<question_id>[0-9]+)/$', views.details, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results')
+    url(r'^(?P<question_id>[0-9]+)/$', views.details, name='details'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/result/$', views.result, name='result')
     #url(r'^deeper/$', views.some_else),
     ]
 #minutul 7:27
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from .models import Question, Choice
-
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -11,14 +13,39 @@ def index(request):
     #Ultimele 5 intrebari sortate pe data
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #le insiram in output cu , intre ele
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def details(request, question_id):
-    return HttpResponse("You're looking at the question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/details.html', {'question' : question})
 
-def results(request, question_id):
-    response = "You are looking at the response of question %s"
-    return HttpResponse(response % question_id)
+def result(request, question_id):
+    #response = "You are looking at the response of question %s"
+    #return HttpResponse(response % question_id)
 #But then again, this is not The Way, we need to create templates, so we
 #create a "templates" folder in pols
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question' : question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html', {
+            'question' : question,
+            'error_message' : 'You did not select a choice',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+
+
