21b1ac334f79c8397f8973dea467d054a596db5c
flavius <flavius.popoiu@mediaimage.ro>
2016-11-17 13:55:44 +0200

    Finished Part 5

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 
 from .models import Question, Choice
-admin.site.register(Question)
+
+'''
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
+'''
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']
+    #to break up the data into blocks you can go
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'],
+        'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
